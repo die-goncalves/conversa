32995da616e61a2b20b20207497e9083d0e60218
die-goncalves <die.goncalves1990@gmail.com>
2023-02-22 16:07:12 -0300

    feat: add LogoSVG component

---
+++ src/components/LogoSVG/styles.ts
@@ -0,0 +1,50 @@
+import styled, { keyframes } from 'styled-components'
+
+const jello = keyframes`
+  0% {
+    -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+  }
+  30% {
+    -webkit-transform: scale3d(1.25, 0.75, 1);
+            transform: scale3d(1.25, 0.75, 1);
+  }
+  40% {
+    -webkit-transform: scale3d(0.75, 1.25, 1);
+            transform: scale3d(0.75, 1.25, 1);
+  }
+  50% {
+    -webkit-transform: scale3d(1.15, 0.85, 1);
+            transform: scale3d(1.15, 0.85, 1);
+  }
+  65% {
+    -webkit-transform: scale3d(0.95, 1.05, 1);
+            transform: scale3d(0.95, 1.05, 1);
+  }
+  75% {
+    -webkit-transform: scale3d(1.05, 0.95, 1);
+            transform: scale3d(1.05, 0.95, 1);
+  }
+  100% {
+    -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+  }
+`
+
+export const LogoSVGContainer = styled.div`
+  line-height: 0;
+  .jello-animation {
+    animation-iteration-count: 1;
+    animation-play-state: running;
+    animation-name: ${jello};
+    animation-duration: 1.2s;
+    animation-timing-function: ease;
+    animation-fill-mode: both;
+    will-change: transform;
+  }
+
+  svg {
+    height: 2.5rem;
+    width: 2.5rem;
+  }
+`
