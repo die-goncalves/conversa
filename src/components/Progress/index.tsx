a93e5c70b86f0042f5ed5834140bf4f38332bc4e
die-goncalves <die.goncalves1990@gmail.com>
2023-04-05 17:11:03 -0300

    refactor(components/progress): add optional id

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -2,11 +2,11 @@ import * as ProgressRadix from '@radix-ui/react-progress'
 import { ProgressContainer } from './styles'
 
 interface IProgressProps {
-  id: string
+  id?: string
 }
 export function Progress({ id }: IProgressProps): JSX.Element {
   return (
-    <ProgressContainer id={id}>
+    <ProgressContainer {...(id !== undefined && { id })}>
       <ProgressRadix.Root className="ProgressRoot" value={null}>
         <ProgressRadix.Indicator className="ProgressIndicator" />
       </ProgressRadix.Root>
