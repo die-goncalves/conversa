fcfff4ace66073d9f595a1122ebb443849e56df9
die-goncalves <die.goncalves1990@gmail.com>
2023-04-20 09:26:44 -0300

    style(components/navlink): add button style to the link

--- src/components/NavLink/styles.ts
+++ src/components/NavLink/styles.ts
@@ -16,38 +16,61 @@ export const NavLinkContainer = styled.div`
       height: 2.5rem;
       object-fit: cover;
     }
+
+    a {
+      text-decoration: none;
+      cursor: pointer;
+
+      transition: opacity 150ms linear, outline-offset 150ms linear;
+      &:hover {
+        opacity: 0.8;
+      }
+      &:focus-visible {
+        border-radius: 1px;
+        outline-style: solid;
+        outline-width: 2px;
+        outline-offset: 2px;
+        outline-color: var(--violet-300);
+      }
+      &.active {
+        text-decoration-line: underline;
+        text-decoration-style: wavy;
+        text-decoration-thickness: 2px;
+        text-decoration-color: var(--violet-300);
+      }
+    }
   }
 
   & > a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid transparent;
+    height: 2.5rem;
+    width: 2.5rem;
+    border-radius: 0.25rem;
+    background: var(--gray-700);
+    cursor: pointer;
+    transition: background-color 150ms linear, border-color 150ms linear,
+      box-shadow 150ms linear;
+
+    box-shadow: var(--shadow);
+    &:hover {
+      background: var(--gray-600);
+      box-shadow: var(--shadow-md);
+    }
+
+    outline: 0;
+    &:focus-visible {
+      border-color: var(--violet-300);
+    }
+
     line-height: 0;
     margin-left: auto;
     svg {
       width: 1.5rem;
       height: 1.5rem;
       fill: var(--violet-50);
-    }
-  }
-
-  a {
-    text-decoration: none;
-    cursor: pointer;
-
-    transition: opacity 150ms linear, outline-offset 150ms linear;
-    &:hover {
-      opacity: 0.8;
-    }
-    &:focus-visible {
-      border-radius: 1px;
-      outline-style: solid;
-      outline-width: 2px;
-      outline-offset: 2px;
-      outline-color: var(--violet-300);
-    }
-    &.active {
-      text-decoration-line: underline;
-      text-decoration-style: wavy;
-      text-decoration-thickness: 2px;
-      text-decoration-color: var(--violet-300);
     }
   }
 `
